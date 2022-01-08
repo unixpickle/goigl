5ae67085a9633498b69e78269d29b0c3c36306b0
Alex Nichol <unixpickle@gmail.com>
2022-01-08 12:37:14 -0800

    interop with model3d

--- interop.go
+++ interop.go
@@ -2,6 +2,21 @@ package goigl
 
 import "github.com/unixpickle/model3d/model3d"
 
+// NewMeshModel3d converts a model3d mesh to a *Mesh.
+func NewMeshModel3d(m *model3d.Mesh) *Mesh {
+	var vertices []float64
+	var faces []int
+	m.Iterate(func(t *model3d.Triangle) {
+		for _, v := range t {
+			idx := len(vertices) / 3
+			faces = append(faces, idx)
+			arr := v.Array()
+			vertices = append(vertices, arr[:]...)
+		}
+	})
+	return NewMesh(vertices, faces)
+}
+
 // Model3d converts the mesh to a model3d mesh.
 func (m *Mesh) Model3d() *model3d.Mesh {
 	vertices := m.Vertices()
