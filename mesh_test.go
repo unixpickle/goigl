6824816992fb31542f302211f5b697a4176d95ef
Alex Nichol <unixpickle@gmail.com>
2022-01-10 08:26:23 -0800

    test mesh duplicate removal

--- mesh_test.go
+++ mesh_test.go
@@ -58,3 +58,37 @@ func TestMeshVerticesFaces(t *testing.T) {
 		}
 	}
 }
+
+func TestMeshRemoveDuplicateVertices(t *testing.T) {
+	data, err := ioutil.ReadFile("test_data/cube.stl")
+	if err != nil {
+		t.Fatal(err)
+	}
+	mesh, err := MeshDecodeSTL(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	m1 := mesh.RemoveDuplicateVertices(0)
+	if m1.NumVertices() != 8 {
+		t.Errorf("expected %d vertices but got %d", 8, m1.NumVertices())
+	}
+	f := mesh.Faces()
+	v := mesh.Vertices()
+	f1 := m1.Faces()
+	v1 := m1.Vertices()
+	if len(f) != len(f1) {
+		t.Fatalf("mismatching face count: %d versus %d", len(f), len(f1))
+	}
+	for i, faceIdx := range f {
+		faceIdx1 := f1[i]
+		vertex := v[faceIdx*3 : (faceIdx+1)*3]
+		vertex1 := v1[faceIdx1*3 : (faceIdx1+1)*3]
+		for i, x := range vertex {
+			y := vertex1[i]
+			if x != y {
+				t.Errorf("face %d: vertex should be %v but got %v", i, vertex, vertex1)
+				break
+			}
+		}
+	}
+}
